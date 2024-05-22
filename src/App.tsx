310b08f5e5cfcee5160d5557d2e4387ef11a5e9b
Sam Estep <sam@samestep.com>
2024-05-22 19:36:03 -0400

    Fix flash when already signed in

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { Login } from "./Login";
 import { auth } from "./client";
 
 const App = () => {
-  const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+  const [user, setUser] = useState(auth.currentUser);
 
   // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
-    const unregisterAuthObserver = auth.onAuthStateChanged((user) => {
-      setIsSignedIn(!!user);
+    const unregisterAuthObserver = auth.onAuthStateChanged((newUser) => {
+      setUser(newUser);
     });
     return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
   }, []);
 
-  return isSignedIn ? <Editor /> : <Login />;
+  return user === null ? <Login /> : <Editor />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.tsx";
+import { auth } from "./client";
 import "./main.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+let started = false;
+
+// check whether the user is signed in before starting
+const unregisterAuthObserver = auth.onAuthStateChanged(() => {
+  if (!started) {
+    started = true;
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root"),
+    );
+  }
+});
+
+// immediately unregister because the above should only run once
+unregisterAuthObserver();
